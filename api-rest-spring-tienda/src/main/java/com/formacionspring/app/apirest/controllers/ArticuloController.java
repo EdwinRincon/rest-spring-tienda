028c2b90db2042c8a6df838ee1f330346d959f8d
punchitox <fjgarciabasanta@gmail.com>
2022-02-11 12:17:27 +0100

    Primer Commit JAvier 2

--- api-rest-spring-tienda/src/main/java/com/formacionspring/app/apirest/controllers/ArticuloController.java
+++ api-rest-spring-tienda/src/main/java/com/formacionspring/app/apirest/controllers/ArticuloController.java
@@ -1,5 +1,14 @@
 package com.formacionspring.app.apirest.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.formacionspring.app.apirest.service.ClienteService;
+
+
+
 public class ArticuloController {
 
+	@Autowired
+	private ClienteService servicio;
+	
 }
